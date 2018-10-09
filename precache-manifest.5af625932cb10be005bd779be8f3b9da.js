b42a0c4197e387da2174380b83e17a6973021ffd
ekhin <waikhin10@gmail.com>
2018-10-09 00:59:56 -0700

    Updates

---
+++ precache-manifest.5af625932cb10be005bd779be8f3b9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "de3de1f78e6eb54e3943",
+    "url": "/Clicky-Game/static/js/main.de3de1f7.chunk.js"
+  },
+  {
+    "revision": "da8f92addef2f6e0c599",
+    "url": "/Clicky-Game/static/js/1.da8f92ad.chunk.js"
+  },
+  {
+    "revision": "de3de1f78e6eb54e3943",
+    "url": "/Clicky-Game/static/css/main.fbcaed67.chunk.css"
+  },
+  {
+    "revision": "da8f92addef2f6e0c599",
+    "url": "/Clicky-Game/static/css/1.5f401146.chunk.css"
+  },
+  {
+    "revision": "35e5b79e8aba17b4bac84460b18be2c5",
+    "url": "/Clicky-Game/index.html"
+  }
+];
